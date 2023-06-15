448f4f017096a44d83ccbedd356d986a0d8656d2
shifatqs <shifatr@questps.com.au>
2023-06-16 09:47:18 +1000

    converted backend to ts

--- controllers/private.js
+++
@@ -1,8 +0,0 @@
-const getPrivateData = (req, res, next) => {
-  return res.status(200).json({
-    success: true,
-    data: "You got access to the private data in this route",
-  });
-};
-
-module.exports = { getPrivateData };
